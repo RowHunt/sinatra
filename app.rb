a6d37aa335ef20fa3f9bc0299a0b737635be1059
RowHunt <watchlist.rh@gmail.com>
2021-02-09 15:41:57 -0500

    Intro to Params

--- app.rb
+++ app.rb
@@ -4,13 +4,21 @@ get '/' do
   'Welp!'
 end
 
+# http://localhost:4567/secret
 get '/secret' do
   'Show!'
 end
 
-get '/cat' do
+get '/random-cat' do
   # We're calling erb, a method from the templating system that ships with Ruby. We are asking it to render a file called index.erb from the folder views
   @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  # We're calling erb, a method from the templating system that ships with Ruby. We are asking it to render a file called index.erb from the folder views
+  p params
+  @name = params[:name]
   erb(:index)
 end
 
